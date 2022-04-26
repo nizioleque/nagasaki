104211ec4556c2b42da28eb98f93eb40b31530a9
nizioleque <nizioleque@gmail.com>
2022-04-26 20:59:19 +0200

    added Field widget, added passing data to/from GameArea and Field, added initState, added tap handling

--- lib/main.dart
+++ lib/main.dart
@@ -25,14 +25,18 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final int rows = 10;
   final int columns = 10;
-  final int nBombs = 10;
+  final int nBombs = 30;
+
+  late List<List<FieldData>> grid;
 
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
+
     // create a 2D table for the grid and initialize with default BombBoxes
-    List<List<BombBox>> grid = List.generate(rows, (i) {
+    grid = List.generate(rows, (i) {
       return List.generate(columns, (j) {
-        return BombBox();
+        return FieldData();
       });
     });
 
@@ -43,12 +47,16 @@ class _MyHomePageState extends State<MyHomePage> {
       grid[index ~/ columns][index % columns].isBomb = true;
     }
 
+    // count bombs
     for (var i = 0; i < rows; i++) {
       for (var j = 0; j < columns; j++) {
         grid[i][j].bombsAround = countBombsAround(grid, i, j);
       }
     }
+  }
 
+  @override
+  Widget build(BuildContext context) {
     var bombsLeft = nBombs;
 
     // return widget
@@ -95,8 +103,26 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: Center(
                       child: AspectRatio(
                         aspectRatio: columns / rows,
-                        child:
-                            GameArea(columns: columns, rows: rows, grid: grid),
+                        child: GameArea(
+                          columns: columns,
+                          rows: rows,
+                          grid: grid,
+                          onChanged: (FieldChangeData data) {
+                            debugPrint(
+                                '[HomePage] onChanged, index: ${data.index}, type: ${data.pressType}');
+                            int i = data.index ~/ columns;
+                            int j = data.index % columns;
+                            switch (data.pressType) {
+                              case PressType.tap:
+                                handleFieldTap(i, j);
+                                break;
+                              case PressType.longPress:
+                                handleFieldLongPress(i, j);
+                                break;
+                              default:
+                            }
+                          },
+                        ),
                       ),
                     ),
                   ),
@@ -109,7 +135,7 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  int countBombsAround(List<List<BombBox>> grid, int i, int j) {
+  int countBombsAround(List<List<FieldData>> grid, int i, int j) {
     int counter = 0;
     if (i - 1 > 0 && j - 1 > 0 && grid[i - 1][j - 1].isBomb) {
       counter++;
@@ -138,45 +164,74 @@ class _MyHomePageState extends State<MyHomePage> {
     return counter;
   }
 
+  handleFieldTap(int i, int j) {
+    debugPrint('[handleFieldTap] $i, $j');
+
+    if (!grid[i][j].isVisible) {
+      setState(() {
+        grid[i][j].isVisible = true;
+      });
+    }
+
+    if (grid[i][j].isBomb) gameOver();
+  }
+
+  handleFieldLongPress(int i, int j) {
+    debugPrint('[handleLongPressTap] $i, $j');
+
+    // mark as bomb / question mark
+  }
+
+  gameOver() {
+    debugPrint('Game Over!');
+  }
+
   resetGame() {}
 
   settings() {}
 }
 
-class BombBox {
+class FieldData {
   bool isBomb;
   bool isVisible;
   int bombsAround;
 
-  BombBox([this.isBomb = false, this.isVisible = false, this.bombsAround = 0]);
+  FieldData([
+    this.isBomb = false,
+    this.isVisible = false,
+    this.bombsAround = 0,
+  ]);
+}
 
-  getWidget() {
-    var fillColor = isBomb ? Colors.orange : Colors.grey;
+class FieldChangeData {
+  int index;
+  PressType pressType;
 
-    return Container(
-      decoration: BoxDecoration(
-        color: fillColor,
-        border: isVisible ? null : Border.all(width: 4.0),
-      ),
-      child: Align(
-        alignment: Alignment.center,
-        child: Text(
-          bombsAround.toString(),
-        ),
-      ),
-    );
-  }
+  FieldChangeData(
+    this.index,
+    this.pressType,
+  );
+}
+
+enum PressType {
+  tap,
+  longPress,
 }
 
 // grid widget class
 class GameArea extends StatelessWidget {
-  const GameArea(
-      {Key? key, required this.columns, required this.rows, required this.grid})
-      : super(key: key);
+  const GameArea({
+    Key? key,
+    required this.columns,
+    required this.rows,
+    required this.grid,
+    required this.onChanged,
+  }) : super(key: key);
 
   final int columns;
   final int rows;
-  final List<List<BombBox>> grid;
+  final List<List<FieldData>> grid;
+  final ValueChanged<FieldChangeData> onChanged;
 
   @override
   Widget build(BuildContext context) {
@@ -187,11 +242,59 @@ class GameArea extends StatelessWidget {
       childAspectRatio: 1,
       children: List.generate(
         rows * columns,
-        (index) => Padding(
-          padding: const EdgeInsets.all(2.0),
-          child: grid[index ~/ columns][index % columns].getWidget(),
+        (index) => Field(
+          data: grid[index ~/ columns][index % columns],
+          handleTap: (bool b) {
+            debugPrint('[GameArea] detected tap');
+            onChanged(FieldChangeData(index, PressType.tap));
+          },
+          handleLongPress: (bool b) {
+            debugPrint('[GameArea] detected long press');
+            onChanged(FieldChangeData(index, PressType.longPress));
+          },
         ),
       ),
+    );
+  }
+}
+
+class Field extends StatelessWidget {
+  const Field({
+    Key? key,
+    required this.handleTap,
+    required this.handleLongPress,
+    required this.data,
+  }) : super(key: key);
+
+  final ValueChanged<bool> handleTap;
+  final ValueChanged<bool> handleLongPress;
+  final FieldData data;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        debugPrint('[Field] detected tap');
+        handleTap(false);
+      },
+      onLongPress: () {
+        debugPrint('[Field] detected long press');
+        handleLongPress(false);
+      },
+      child: Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: data.isBomb ? Colors.orange : Colors.grey,
+              border: data.isVisible ? null : Border.all(width: 4.0),
+            ),
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                data.bombsAround.toString(),
+              ),
+            ),
+          )),
     );
   }
 }
