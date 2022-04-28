cf4333f76cf2822fd15fd1cd9f53a429d45ebbaa
nizioleque <nizioleque@gmail.com>
2022-04-28 15:13:32 +0200

    Simplified field iterator

--- lib/iterators.dart
+++ lib/iterators.dart
@@ -5,80 +5,26 @@ import 'grid.dart';
 
 class FieldsAroundIterator extends Iterator<FieldData> {
   late Grid grid;
-  late FieldPosition pos;
-  late int endi;
-  late int endj;
-  int returned = -1;
-  // late FieldData _current;
+  // late FieldPosition pos;
+  // late int endi;
+  // late int endj;
+  // int returned = -1;
+
+  late FieldsAroundPositionIterator iter;
 
   FieldsAroundIterator({required this.grid, required index}) {
-    pos = FieldPosition(index, grid.columns);
-    endi = grid.rows - 1;
-    endj = grid.columns - 1;
-    // _current = g.atIndex(index);
+    // pos = FieldPosition(index, grid.columns);
+    // endi = grid.rows - 1;
+    // endj = grid.columns - 1;
+
+    iter = FieldsAroundPositionIterator(grid: grid, index: index);
   }
 
   @override
-  FieldData get current {
-    switch (returned) {
-      case 0:
-        return grid.atij(pos.i - 1, pos.j);
-      case 1:
-        return grid.atij(pos.i - 1, pos.j + 1);
-      case 2:
-        return grid.atij(pos.i, pos.j + 1);
-      case 3:
-        return grid.atij(pos.i + 1, pos.j + 1);
-      case 4:
-        return grid.atij(pos.i + 1, pos.j);
-      case 5:
-        return grid.atij(pos.i + 1, pos.j - 1);
-      case 6:
-        return grid.atij(pos.i, pos.j - 1);
-      case 7:
-        return grid.atij(pos.i - 1, pos.j - 1);
-      default:
-        return grid.atPos(pos);
-    }
-  }
+  FieldData get current => grid.atIndex(iter.current);
 
   @override
-  bool moveNext() {
-    returned++;
-
-    switch (returned) {
-      case 0:
-        if (pos.i == 0) {
-          return moveNext();
-        }
-        break;
-      case 1:
-        if (pos.i == 0 || pos.j == endj) return moveNext();
-        break;
-      case 2:
-        if (pos.j == endj) return moveNext();
-        break;
-      case 3:
-        if (pos.i == endi || pos.j == endj) return moveNext();
-        break;
-      case 4:
-        if (pos.i == endi) return moveNext();
-        break;
-      case 5:
-        if (pos.i == endi || pos.j == 0) return moveNext();
-        break;
-      case 6:
-        if (pos.j == 0) return moveNext();
-        break;
-      case 7:
-        if (pos.i == 0 || pos.j == 0) return moveNext();
-        break;
-      default:
-        return false;
-    }
-
-    return true;
-  }
+  bool moveNext() => iter.moveNext();
 }
 
 class FieldsAround extends IterableBase<FieldData> {
