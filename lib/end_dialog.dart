e1eb97f474f92f6a20b105540d973b890c9da030
nizioleque <nizioleque@gmail.com>
2022-05-07 22:23:43 +0200

    Started working on new end game dialog

--- lib/end_dialog.dart
+++ lib/end_dialog.dart
@@ -3,14 +3,46 @@ import 'package:flutter/material.dart';
 import 'grid.dart';
 
 showEndGameDialog(BuildContext context, Grid grid, bool success) async {
-  showDialog<String>(
+  // showDialog<String>(
+  //   context: context,
+  //   builder: (BuildContext context) => AlertDialog(
+  //     title: success ? const Text("YOU WON") : const Text("You lost"),
+  //     content: success
+  //         ? const Text("You've found all bombs!")
+  //         : const Text("You've hit a bomb!"),
+  //     backgroundColor: success ? Colors.lightGreen : Colors.red,
+  //   ),
+  // );
+
+  await showDialog(
     context: context,
     builder: (BuildContext context) => AlertDialog(
-      title: success ? const Text("YOU WON") : const Text("You lost"),
-      content: success
-          ? const Text("You've found all bombs!")
-          : const Text("You've hit a bomb!"),
-      backgroundColor: success ? Colors.lightGreen : Colors.red,
+      title: const Text("You Lost"),
+      contentPadding: const EdgeInsets.all(24.0),
+      scrollable: true,
+      content: Column(
+        children: [
+          Row(children: const [Text("Time"), Text("100")]),
+          Row(children: const [Text("Bombs disarmed"), Text("69")]),
+          Row(children: const [Text("False positives"), Text("20")]),
+          Row(children: const [Text("Bombs exploded"), Text("5")]),
+        ],
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text("Close"),
+        ),
+        TextButton(
+          onPressed: () {
+            // new game
+            Navigator.of(context).pop();
+          },
+          child: const Text("New Game"),
+        ),
+      ],
     ),
   );
 }
