c492a57b189be88066b8ec8e894501feccc5c1e0
nizioleque <nizioleque@gmail.com>
2022-05-08 12:51:18 +0200

    Added radio buttons to settings menu

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:nagasaki/classes.dart';
-import 'package:nagasaki/widgets/game_area.dart';
 
+import 'classes.dart';
 import 'grid.dart';
 import 'helpers.dart';
 import 'widgets/dialog.dart';
+import 'widgets/settings.dart';
 
 Future<List> openSettings(Grid grid, BuildContext context) async {
   final _controllers = [for (var i = 0; i < 3; i++) TextEditingController()];
@@ -43,34 +43,7 @@ Future<List> openSettings(Grid grid, BuildContext context) async {
       title: "Settings",
       content: Form(
         key: _formKey,
-        child: Column(
-          // mainAxisSize: MainAxisSize.min,
-          children: [
-            SettingsNumberField(
-              controller: _controllers[0],
-              validator: (value) => validateRange(value, 3, 30),
-              text: "Columns",
-            ),
-            SettingsNumberField(
-              controller: _controllers[1],
-              validator: (value) => validateRange(value, 3, 30),
-              text: "Rows",
-            ),
-            SettingsNumberField(
-                controller: _controllers[2],
-                validator: (value) {
-                  try {
-                    var fields = int.parse(_controllers[0].text) *
-                        int.parse(_controllers[1].text);
-
-                    return validateRange(value, 2, fields - 9);
-                  } catch (e) {
-                    return null;
-                  }
-                },
-                text: "Mines"),
-          ],
-        ),
+        child: SettingsDialogContent(controllers: _controllers),
       ),
       actions: [
         TextButton(
@@ -99,4 +72,133 @@ Future<List> openSettings(Grid grid, BuildContext context) async {
   );
 
   return [settingsChanged, newSett];
+}
+
+class SettingsDialogContent extends StatefulWidget {
+  const SettingsDialogContent({
+    Key? key,
+    required List<TextEditingController> controllers,
+  })  : _controllers = controllers,
+        super(key: key);
+
+  final List<TextEditingController> _controllers;
+
+  @override
+  State<SettingsDialogContent> createState() => _SettingsDialogContentState();
+}
+
+enum Difficulty { beginner, intermediate, expert, custom }
+
+class _SettingsDialogContentState extends State<SettingsDialogContent> {
+  Difficulty _difficulty = Difficulty.beginner;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      // mainAxisSize: MainAxisSize.min,
+      children: [
+        DifficultyRadio(
+          label: 'Beginner',
+          value: Difficulty.beginner,
+          groupValue: _difficulty,
+          onChanged: (Difficulty newValue) {
+            setState(() {
+              _difficulty = newValue;
+            });
+          },
+        ),
+        DifficultyRadio(
+          label: 'Intermediate',
+          value: Difficulty.intermediate,
+          groupValue: _difficulty,
+          onChanged: (Difficulty newValue) {
+            setState(() {
+              _difficulty = newValue;
+            });
+          },
+        ),
+        DifficultyRadio(
+          label: 'Expert',
+          value: Difficulty.expert,
+          groupValue: _difficulty,
+          onChanged: (Difficulty newValue) {
+            setState(() {
+              _difficulty = newValue;
+            });
+          },
+        ),
+        DifficultyRadio(
+          label: 'Custom',
+          value: Difficulty.custom,
+          groupValue: _difficulty,
+          onChanged: (Difficulty newValue) {
+            setState(() {
+              _difficulty = newValue;
+            });
+          },
+        ),
+        SettingsNumberField(
+          controller: widget._controllers[0],
+          validator: (value) => validateRange(value, 3, 30),
+          text: "Columns",
+        ),
+        SettingsNumberField(
+          controller: widget._controllers[1],
+          validator: (value) => validateRange(value, 3, 30),
+          text: "Rows",
+        ),
+        SettingsNumberField(
+            controller: widget._controllers[2],
+            validator: (value) {
+              try {
+                var fields = int.parse(widget._controllers[0].text) *
+                    int.parse(widget._controllers[1].text);
+
+                return validateRange(value, 2, fields - 9);
+              } catch (e) {
+                return null;
+              }
+            },
+            text: "Mines"),
+      ],
+    );
+  }
+}
+
+class DifficultyRadio extends StatelessWidget {
+  const DifficultyRadio({
+    Key? key,
+    required this.label,
+    required this.groupValue,
+    required this.value,
+    required this.onChanged,
+  }) : super(key: key);
+
+  final String label;
+  final Difficulty groupValue;
+  final Difficulty value;
+  final ValueChanged<Difficulty> onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        if (value != groupValue) {
+          onChanged(value);
+        }
+      },
+      child: Row(
+        children: <Widget>[
+          Radio<Difficulty>(
+            groupValue: groupValue,
+            value: value,
+            onChanged: (Difficulty? newValue) {
+              onChanged(newValue!);
+            },
+          ),
+          Text(label),
+        ],
+      ),
+    );
+  }
 }
--- lib/widgets/game_area.dart
+++ lib/widgets/game_area.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import '../classes.dart';
 import '../grid.dart';
 
@@ -162,31 +161,5 @@ class FieldImage extends StatelessWidget {
   }
 }
 
-// settings dialog number fields
-class SettingsNumberField extends StatelessWidget {
-  const SettingsNumberField({
-    Key? key,
-    required this.controller,
-    required this.validator,
-    required this.text,
-  }) : super(key: key);
 
-  final TextEditingController controller;
-  final String? Function(String?) validator;
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      autovalidateMode: AutovalidateMode.always,
-      validator: validator,
-      controller: controller,
-      keyboardType: TextInputType.number,
-      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-      decoration: InputDecoration(
-        labelText: text,
-      ),
-    );
-  }
-}
 
